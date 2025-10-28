65f3ef4c12d8a2e5a27d84aef2bbd2bbce8374fd
73107 <73107@student.wsiz.edu.pl>
2025-10-28 11:50:43 +0100

    initial commit

---
+++ lab1/zad1.py
@@ -0,0 +1,19 @@
+imie = "Maksym"
+kierunek = "cyberbiezpiecienstwo"
+uczelnia = "WSIiZ"
+# sposob 1
+print("Nazywam sie")
+print(imie)
+# sposob 2
+print("Nazywam sie "+imie )
+#sposob 3
+print("Nazywam sie", imie)
+#dokumentacja dot. print i argumentow
+#sposob 4
+print(f"Nazywam sie {imie}")
+#*
+print("Nazywam sie", imie, sep="_", end=" ")
+print("cos")
+
+
+print("nazywam sie",imie, ",chodze na uczelnie", uczelnia, ",moj kierunek to", kierunek )
